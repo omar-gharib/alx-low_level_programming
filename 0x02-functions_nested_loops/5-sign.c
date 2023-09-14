854342f39249179869e26109ce4433645966d248
Omar Gharib <141337251+omar-gharib@users.noreply.github.com>
2023-09-14 12:27:28 -0700

    Debugging

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,20 +6,24 @@
  * Return: 1 and prints + if n is greater than zero
  * 0 and prints 0 if n is zero
  * -1 and prints - if n is less than zero
- */
+*/
 int print_sign(int n)
 {
 	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
-	} else if (n == 0)
-}
+	}
+	else if (n == 0)
+	{
 		_putchar(48);
 		return (0);
-	} else if (n < 0)
-	{
-		_putchar('-');
 	}
-		return (-1);
+	else if (n < 0)
+	{
+	else
+	}
+		_putchar(45);
+	{
+	return (-1);
 }
