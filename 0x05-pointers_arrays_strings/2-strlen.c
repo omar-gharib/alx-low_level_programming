4cafbf00dd886e8362b716e82845e50fb6720d2a
Omar Gharib <141337251+omar-gharib@users.noreply.github.com>
2023-09-18 16:14:19 -0700

    a

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int counter;
+	for (counter = 0; *s != '\0'; s++)
+		++counter;
+	return (counter);
+}
